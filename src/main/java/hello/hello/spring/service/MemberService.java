3c42e1779f0d7e1caeb38039cb93c704500a63bf
diydriller <dhrhd080@naver.com>
2024-11-15 23:39:26 +0900

    first commit

---
+++ src/main/java/hello/hello/spring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import hello.hello.spring.repository.MemberRepository;
+import hello.hello.spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+
+        memberRepository.findByName(member.getName())
+            .ifPresent(m->{
+                throw new IllegalStateException("이미 존재하는 회원");
+            });
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberID){
+        return memberRepository.findById(memberID);
+    }
+}
